438d561181a30d739f20557407d40aabf9e50ada
Nikki Murello <nmurello15@gmail.com>
2026-03-29 18:22:36 -0400

    Added dog model, updated DB data dir

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,7 @@
-module.exports.Cat = require('./Cat.js');
+module.exports = {
+    Cat: require("./Cat.js"),
+    Dog: require("./Dog.js")
+}
 
 // index.js gets automatically pulled when a folder is required
 // We use this to our advantage to define an API for this package.
